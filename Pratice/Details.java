15a3b50b910b4eb2145d0bfb2935c5f26ec8fb28
Rok-dotcom <kolirameshshivaji@gmail.com>
2026-05-22 12:53:33 +0530

    Added Practice folder

---
+++ Pratice/Details.java
@@ -0,0 +1,11 @@
+// 22 May 2026 : Solapur library LOKRAJYA ABHYASIKA.
+
+/*
+This folder created for practice of java only
+ */
+
+public class Details {
+    public static void main(String[] args){
+        System.out.println("just checking ");
+    }
+}
