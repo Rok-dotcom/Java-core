499e9f1304a9b89d4872006cdad97479e9491e42
Rok-dotcom <kolirameshshivaji@gmail.com>
2026-06-25 21:14:31 +0530

    lc

---
+++ Pratice/Streams/normalMethod.java
@@ -0,0 +1,13 @@
+package Streams;
+
+public class normalMethod {
+    public static void main(String[] args){
+        int a = 0;
+        int b = 0;
+        System.out.println(sum(a,b));
+
+    }
+    static int sum(int a, int b){
+        return a+b;
+    }
+}
