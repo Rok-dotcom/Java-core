a9e1cb750a0488131b80de6a23a0f205c6d4b774
Rok-dotcom <kolirameshshivaji@gmail.com>
2026-07-14 21:30:32 +0530

    lc

---
+++ Pratice/LeetCode/SubSet_78.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class SubSet_78 {
+    public static void main(String[] args){
+        List<Integer> list = new LinkedList<>();
+        list.add(1);
+        System.out.println(list);
+    }
+}
