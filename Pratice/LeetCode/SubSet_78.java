50ad6e6b7aaa6406e32449fa2ce668d68be3e172
Rok-dotcom <kolirameshshivaji@gmail.com>
2026-07-16 19:28:15 +0530

    lc

--- Pratice/LeetCode/SubSet_78.java
+++ Pratice/LeetCode/SubSet_78.java
@@ -1,6 +1,9 @@
 import java.util.LinkedList;
 import java.util.List;
 
+// 16 July 2026
+// solved tree Problem in LC
+
 public class SubSet_78 {
     public static void main(String[] args){
         List<Integer> list = new LinkedList<>();
